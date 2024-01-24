e5b8f331694bfafa0b75c104fa5b24d217be15f6
= <=>
2024-01-24 15:46:56 +0530

    Implemented ghost elements

--- kokkos/simulation.cpp
+++ kokkos/simulation.cpp
@@ -7,10 +7,20 @@ constexpr double L = 1.0;         // Domain length
 constexpr double T = 0.1;         // Simulation time
 constexpr double alpha = 1.0;      // Diffusion coefficient
 constexpr double sigma = 0.15;     // CFL stability number
+constexpr int Nbuf = 3;            // Number of buffer cells
 
 // Initial condition function
 double initial_condition(double x) {
     return std::sin(2.0 * M_PI * x);
+}
+
+// function to update ghost cells, pass by reference
+void update_ghost_cells(Kokkos::View<double*> u, const int N) {
+    // Update ghost cells
+    Kokkos::parallel_for("UpdateGhostCells", Nbuf, KOKKOS_LAMBDA(const int i) {
+        u(i) = u(i + N);
+        u(i + N + Nbuf) = u(i + Nbuf);
+    });
 }
 
 int main(int argc, char* argv[]) {
@@ -32,19 +42,22 @@ int main(int argc, char* argv[]) {
     {
 
         // Define views for Kokkos
-        Kokkos::View<double*> u("u", N);
-        Kokkos::View<double**> u_3d("u_3d", num_steps, N);
+        Kokkos::View<double*> u("u", N + 2*Nbuf);
+        // Kokkos::View<double**> u_3d("u_3d", num_steps, N);
         Kokkos::View<double*> errors("errors", num_steps);
-        Kokkos::View<double*> u_frac("u_frac", N);
+        Kokkos::View<double*> u_frac("u_frac", N + 2*Nbuf);
 
         // Initialize initial condition
         Kokkos::parallel_for("Initialize", N, KOKKOS_LAMBDA(const int i) {
             double x = i * dx;
-            u(i) = initial_condition(x);
+            u(i + Nbuf) = initial_condition(x);
         });
 
+        // Update ghost cells
+        update_ghost_cells(u, N);
+
         // copy u to u_frac
-        Kokkos::parallel_for("Copy", N, KOKKOS_LAMBDA(const int i) {
+        Kokkos::parallel_for("Copy", N+2*Nbuf, KOKKOS_LAMBDA(const int i) {
             u_frac(i) = u(i);
         });
 
@@ -52,9 +65,9 @@ int main(int argc, char* argv[]) {
         for (int step = 0; step < num_steps; ++step) {
 
             // Copy data to u_3d
-            Kokkos::parallel_for("CopyToResult", N, KOKKOS_LAMBDA(const int i) {
-                u_3d(step, i) = u(i);
-            });
+            // Kokkos::parallel_for("CopyToResult", N, KOKKOS_LAMBDA(const int i) {
+            //     u_3d(step, i) = u(i + Nbuf);
+            // });
         
             // Write results to a file results_N.txt
             if (step == time_steps_to_save[0] || step == time_steps_to_save[1] || step == time_steps_to_save[2] || step == time_steps_to_save[3] || step == time_steps_to_save[4]) {
@@ -64,7 +77,7 @@ int main(int argc, char* argv[]) {
                 else 
                     outFile.open("results_" + std::to_string(N) + ".txt", std::ios_base::app);
                 for (int i = 0; i < N; ++i) {
-                    outFile << u(i) << " ";
+                    outFile << u(i + Nbuf) << " ";
                 }
                 outFile << "\n";
                 outFile.close();
@@ -74,7 +87,8 @@ int main(int argc, char* argv[]) {
             double error = 0.0;
             Kokkos::parallel_reduce("Error", N, KOKKOS_LAMBDA(const int i, double& error) {
                 double x = i * dx;
-                double diff = std::abs(u(i) - std::exp(-4.0 * M_PI * M_PI * alpha * step * dt) * std::sin(2.0 * M_PI * x));
+                int i_new = i + Nbuf;
+                double diff = std::abs(u(i_new) - std::exp(-4.0 * M_PI * M_PI * alpha * step * dt) * std::sin(2.0 * M_PI * x));
                 error += diff;
             }, error);
             errors(step) = error/N;
@@ -90,34 +104,31 @@ int main(int argc, char* argv[]) {
 
             for (int m = 0; m < 3; m++) {
 
-                Kokkos::View<double*> u_temp("u_temp", N);
+                Kokkos::View<double*> u_temp("u_temp", N + 2*Nbuf);
 
                 Kokkos::parallel_for("Diffusion", N, KOKKOS_LAMBDA(const int i) {
-                    int left1 = (i - 1 + N) % N;
-                    int left2 = (i - 2 + N) % N;
-                    int left3 = (i - 3 + N) % N;
-                    int right1 = (i + 1) % N;
-                    int right2 = (i + 2) % N;
-                    int right3 = (i + 3) % N;
-
-                    q(i) = a[m]*q(i) + alpha * (2*u_frac(left3) - 27*u_frac(left2) + 270*u_frac(left1) - 490*u_frac(i) + 270*u_frac(right1) - 27*u_frac(right2) + 2*u_frac(right3)) / (180*dx*dx) * dt;
-                    u_temp(i) = u_frac(i) + b[m]*q(i);
+                    int i_new = i + Nbuf;
+                    q(i) = a[m]*q(i) + alpha * (2*u_frac(i_new-3) - 27*u_frac(i_new-2) + 270*u_frac(i_new-1) - 490*u_frac(i_new) + 270*u_frac(i_new+1) - 27*u_frac(i_new+2) + 2*u_frac(i_new+3)) / (180*dx*dx) * dt;
+                    u_temp(i_new) = u_frac(i_new) + b[m]*q(i);
 
                 });
 
                 Kokkos::fence();
 
                 // copy u_temp to u_frac
-                Kokkos::parallel_for("Copy", N, KOKKOS_LAMBDA(const int i) {
+                Kokkos::parallel_for("Copy", N+2*Nbuf, KOKKOS_LAMBDA(const int i) {
                     u_frac(i) = u_temp(i);
                 });
+
+                // Update ghost cells
+                update_ghost_cells(u_frac, N);
 
                 Kokkos::fence();
 
             }
 
             // copy u_frac to u
-            Kokkos::parallel_for("Copy", N, KOKKOS_LAMBDA(const int i) {
+            Kokkos::parallel_for("Copy", N+2*Nbuf, KOKKOS_LAMBDA(const int i) {
                 u(i) = u_frac(i);
             });
 
